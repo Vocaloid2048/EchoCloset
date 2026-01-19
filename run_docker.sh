3a1d213826fa7d64ccb90924aa00f66ae7ef25e0
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2026-01-19 19:49:51 +0800

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,4 +4,4 @@
 docker build -t echo-closet .
 
 # Run the container with .env mounted
-docker run --env-file .env -v $(pwd)/echo_closet_records.json:/app/echo_closet_records.json echo-closet
+docker run --name echo-closet-bot --env-file .env -v $(pwd)/echo_closet_records.json:/app/echo_closet_records.json echo-closet
